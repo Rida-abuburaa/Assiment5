eb11147b4760ff9b04d15e924a9892d3b6587010
Rida Abuburaa <ridaabuburaa@gmail.com>
2026-08-02 16:03:13 -0700

    Question1

--- Assiment5/Program.cs
+++ Assiment5/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Assiment5
+﻿using Assiment5.istruct;
+
+namespace Assiment5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question1
+
+            //a) What happens when a DeliveryAddress variable is copied into another variable and the copy is modified? 
+            //DeliveryAddress is a struct, which is a value type.
+            // When a DeliveryAddress variable is copied, a separate copy of all its fields is created. Any modifications to the copied variable do not affect the original variable.
+
+            //b) What happens when a Customer variable is copied into another variable and one variable modifies the object? 
+            //Customer is a class, which is a reference type.
+            //When a Customer variable is copied, both variables reference the same object. If one variable modifies the object's fields, the changes are reflected when accessing the object through the other variable.
+
+
+
+
+            #endregion
+
+
+
+
         }
     }
 }
---
+++ Assiment5/struct/DeliveryAddress.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assiment5.istruct
+{
+    public struct DeliveryAddress
+    {
+        public string City;
+        public string Street;
+    }
+    public class Customer
+    {
+        public string Name;
+    }
+}
