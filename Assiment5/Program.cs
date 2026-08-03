3f06cb685d04bf4fc6ebef07dc543c50deae9f67
Rida Abuburaa <ridaabuburaa@gmail.com>
2026-08-02 17:06:45 -0700

    Question2

--- Assiment5/Program.cs
+++ Assiment5/Program.cs
@@ -21,9 +21,18 @@ namespace Assiment5
 
             #endregion
 
+            #region Question2
+            //a) Identify at least three problems with this design from an encapsulation perspective. 
+            //1.All fields (Description, Weight, and DeliveryFee) are public, so they can be accessed and modified directly from outside the class.
+            //2.There is no validation for the data. For example, Weight or DeliveryFee could be assigned invalid values such as negative numbers.
+            //3.The class does not protect its internal data, which breaks the principle of encapsulation.
 
+            // b) How can private fields and public properties improve this design?
+            //Make the fields private so they cannot be accessed directly from outside the class.
+            //Properties can validate the data before assigning values (for example, ensuring that Weight and DeliveryFee are not negative). 
+            //This improves encapsulation, protects the object's data, and makes the class easier to maintain.
 
-
+            #endregion
         }
     }
 }
